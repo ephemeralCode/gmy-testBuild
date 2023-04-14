d593355b8cae0f223bd47120c95bb61a738480f7
ephemeralCode <destroid707@yandex.ru>
2023-04-14 16:16:52 +0300

    added animation text & fixed images portfolioPage

---
+++ js/animatrionTextScroll.js
@@ -0,0 +1,28 @@
+let options = {
+    root: null,
+    rootMargin: '5px',
+    threshold: .5
+}
+
+function callback(entries, observer) {
+    entries.forEach(elem => {
+        if (elem.isIntersecting) {
+            console.log(elem)
+
+            elem.target.classList.add('active')
+        }
+    })
+}
+
+let observer = new IntersectionObserver(callback, options)
+
+let upAnim = document.querySelectorAll('.upAnimationText')
+let downAnim = document.querySelectorAll('.downAnimationText')
+
+upAnim.forEach(elem => {
+    observer.observe(elem)
+})
+
+downAnim.forEach(elem => {
+    observer.observe(elem)
+})
